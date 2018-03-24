c4930ff32ce60a49ae2acde63cb386cd21e4270f
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-03-24 12:13:48 -0400

    general-update-7
    edit to basket cage overlay z-index timing

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
     $("#disc").addClass("madePutt");
     setTimeout(function() {
       $("#disc").addClass("madePuttDrop");
+      $("#cage").addClass("cageOverlay");
     }, 1050);
 
     setTimeout(function() {
       $("#disc").removeClass("madePutt");
       $("#disc").removeClass("madePuttDrop");
+      $("#cage").removeClass("cageOverlay");
     }, 2250);
 
   })
