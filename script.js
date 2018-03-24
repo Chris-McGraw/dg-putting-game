105c72edeaa9a7eab2773adadc0005678fc7c2ce
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-03-24 11:01:32 -0400

    general-update-5

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ $(document).ready(function() {
     setTimeout(function() {
       $("#disc").addClass("madePuttDrop");
     }, 1050);
+
+    setTimeout(function() {
+      $("#disc").removeClass("madePutt");
+      $("#disc").removeClass("madePuttDrop");
+    }, 2250);
   })
 });
