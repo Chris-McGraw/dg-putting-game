3f20ebae69ae928820702fe6074b43c79b2fe183
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-03-25 10:45:11 -0400

    script-update-2

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ $(document).ready(function() {
   $("#disc").draggable({
     axis: "y",
     stop: function( event, ui ) {
+
+      /* console.log($("#disc").position()); */
+
       $("#disc").addClass("madePutt");
 
       setTimeout(function() {
         $("#disc").addClass("madePuttDrop");
         $("#cage").addClass("cageOverlay");
-      }, 1050);
+      }, 1100);
+
+      /* setTimeout(function() {
+        console.log($("#disc").position());
+      }, 1100); */
 
       setTimeout(function() {
         $("#disc").removeClass("madePutt");
