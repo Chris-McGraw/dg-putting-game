4eeaa2e13d6b4498df44a019579fbbca89255dcd
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-03-24 20:48:03 -0400

    script-update-1
    added jQuery UI to enable initial draggability on #disc

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-  $("#disc").on("click", function() {
-    $("#disc").addClass("madePutt");
-    setTimeout(function() {
-      $("#disc").addClass("madePuttDrop");
-      $("#cage").addClass("cageOverlay");
-    }, 1050);
 
-    setTimeout(function() {
-      $("#disc").removeClass("madePutt");
-      $("#disc").removeClass("madePuttDrop");
-      $("#cage").removeClass("cageOverlay");
-    }, 2250);
+  $("#disc").draggable({
+    axis: "y",
+    stop: function( event, ui ) {
+      $("#disc").addClass("madePutt");
 
-  })
+      setTimeout(function() {
+        $("#disc").addClass("madePuttDrop");
+        $("#cage").addClass("cageOverlay");
+      }, 1050);
+
+      setTimeout(function() {
+        $("#disc").removeClass("madePutt");
+        $("#disc").removeClass("madePuttDrop");
+        $("#cage").removeClass("cageOverlay");
+      }, 2250);
+    }
+  });
+
 });
