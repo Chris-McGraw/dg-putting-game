4f7213f22fd915ac53d7801fd9730e981827b38d
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-03-28 11:10:56 -0400

    script-update-10

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
             setTimeout(function() {
               var dropHeight = powerChange + -480;
-              $("#disc").addClass("made-putt-drop");
+              $("#disc").addClass("putt-drop");
               $("#disc").css({"transform": "translate(18px," + dropHeight + "px) rotate(0deg)"});
               $("#cage").addClass("cage-overlay");
             }, 1100);
@@ -35,8 +35,41 @@ $(document).ready(function() {
 
             setTimeout(function() {
               $("#disc").removeClass("putt");
-              $("#disc").removeClass("made-putt-drop");
+              $("#disc").removeClass("putt-drop");
               $("#cage").removeClass("cage-overlay");
+              $("#disc").addClass("disc-start");
+              $("#disc").css({"transform": "translate(0px, 0px) rotate(0deg)"});
+            }, 2250);
+          }
+        }
+      }
+
+  /* ---------------------- Missed Putt High ---------------------- */
+      if($("#disc").position().top >= 76 && $("#disc").position().top <= 110) {
+        for(shotPower = 76; shotPower <= 110; shotPower++) {
+          if($("#disc").position().top === shotPower) {
+            var powerChange = 76 - shotPower;
+            console.log(powerChange);
+            var shotHeight = powerChange + -580 + powerChange + powerChange;
+            console.log(shotHeight);
+            $("#disc").addClass("putt");
+            $("#disc").css({"transform": "translate(18px," + shotHeight + "px) rotate(-15deg)"});
+
+            setTimeout(function() {
+              var dropHeight = powerChange + -456;
+              $("#disc").addClass("putt-drop");
+              $("#disc").css({"transform": "translate(18px," + dropHeight + "px) rotate(0deg)"});
+              /* $("#cage").addClass("cage-overlay"); */
+            }, 1100);
+
+            setTimeout(function() {
+              console.log($("#disc").position());
+            }, 1100);
+
+            setTimeout(function() {
+              $("#disc").removeClass("putt");
+              $("#disc").removeClass("putt-drop");
+              /* $("#cage").removeClass("cage-overlay"); */
               $("#disc").addClass("disc-start");
               $("#disc").css({"transform": "translate(0px, 0px) rotate(0deg)"});
             }, 2250);
