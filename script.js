0b29f6cc340b0a75197c301f0b6fcad2b15c1524
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-05-02 13:32:29 -0400

    script-update-73
    http secure update

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ $(document).ready(function() {
 
   function resizeBasket() {
     if($(window).height() <= 675) {
-      $basket.attr("src", "http://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_80/v1511904652/dg-basket-edit_Resize_pnxwcj.png");
-      $cage.attr("src", "http://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_78/v1521906280/dg-basket-only_zirrcr.png");
+      $basket.attr("src", "https://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_80/v1511904652/dg-basket-edit_Resize_pnxwcj.png");
+      $cage.attr("src", "https://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_78/v1521906280/dg-basket-only_zirrcr.png");
     }
     else {
       $basket.attr("src", "https://res.cloudinary.com/dtwyohvli/image/upload/v1511904652/dg-basket-edit_Resize_pnxwcj.png");
