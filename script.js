d42932fd3172eb3759d19ab4a44c1d97b78569a2
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-05-06 16:15:00 -0400

    script-update-82
    chainHitAnimation() test

--- script.js
+++ script.js
@@ -54,14 +54,14 @@ $(document).ready(function() {
   function chainHitAnimation() {
     $basket.attr("src", "https://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_80/v1525625856/dg-basket-chain-hit-1_rpzvvt.png");
     setTimeout(function() {
-      $basket.attr("src", "http://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_80/v1525623498/dg-basket-chain-hit-test_xcpgr2.png");
-    }, 150);
-    setTimeout(function() {
+      $basket.attr("src", "https://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_80/v1511904652/dg-basket-edit_Resize_pnxwcj.png");
+    }, 200);
+    /* setTimeout(function() {
       $basket.attr("src", "https://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_80/v1525625856/dg-basket-chain-hit-2_opgt7j.png");
     }, 300);
     setTimeout(function() {
       $basket.attr("src", "https://res.cloudinary.com/dtwyohvli/image/upload/c_scale,w_80/v1511904652/dg-basket-edit_Resize_pnxwcj.png");
-    }, 450);
+    }, 450); */
   }
 
 
